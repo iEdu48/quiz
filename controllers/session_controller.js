142b0eddeefbfc4d3d8a2f8b4ec985fa19c126dd
iEdu48 <edugarfol@gmail.com>
2015-07-15 22:48:33 +0200

    Autorización
    Oculta los botones editar y borrar, mientras el usuario no está logeado

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
@@ -29,7 +38,7 @@ exports.create = function(req, res) {
     });
 };
 
-// DELETE /logout   -- Destruir sesion
+// DELETE /logout   -- Destruir sesion 
 exports.destroy = function(req, res) {
     delete req.session.user;
     res.redirect(req.session.redir.toString()); // redirect a path anterior a login
